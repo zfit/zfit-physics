41b8f171a492619cc8bde31faf760b5eb1b38c7b
Jonas Eschle 'Mayou36 <mayou36@jonas.eschle.com>
2019-03-30 23:02:09 +0100

    Initial setup of project

---
+++ docs/make_docs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# script has to be executed inside folder `docs`
+# get current directory name
+pushd `dirname $0` > /dev/null
+MAKE_DOCS_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
+popd > /dev/null
+
+# generate the ReST files
+echo "debug"
+echo ${MAKE_DOCS_PATH}/../zfit_physics
+ls ${MAKE_DOCS_PATH}
+ls .
+sphinx-apidoc -o ${MAKE_DOCS_PATH}/api ${MAKE_DOCS_PATH}/../zfit_physics -fMeT && \
+python3 ${MAKE_DOCS_PATH}/api/tools/change_headline.py ${MAKE_DOCS_PATH}/api/zfit_physics.* && \
+make -C ${MAKE_DOCS_PATH} clean && make -C ${MAKE_DOCS_PATH} html -j8 && \
+echo "Documentation successfully built!" || echo "FAILED to build Documentation"
---
+++ zfit_physics/__init__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""Top-level package for zfit."""
+
+__version__ = '0.0.1'
+
+__license__ = "BSD 3-Clause"
+__copyright__ = "Copyright 2019, zfit"
+__status__ = "Pre-alpha"
+
+__author__ = "zfit"
+__maintainer__ = "zfit"
+__email__ = 'zfit@physik.uzh.ch'
+__credits__ = ["Jonas Eschle <jonas.eschle@cern.ch>",
+               "Albert Puig <albert.puig@cern.ch",
+               "Rafael Silva Coutinho <rafael.silva.coutinho@cern.ch>",
+               # TODO(release): add more, Anton etc
+               ]
+
+__all__ = []
