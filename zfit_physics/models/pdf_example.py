0db3c33d91d0e4555c5ed4aedd11f218783f8e07
Jonas Eschle 'Mayou36 <mayou36@jonas.eschle.com>
2019-04-16 15:07:46 +0200

    Add framework skeleton

---
+++ zfit_physics/models/pdf_example.py
@@ -0,0 +1,11 @@
+"""Example file of a custom pdf implementation. Create a modul for each pdf that you add."""
+import zfit
+
+class Example(zfit.pdf.ZPDF):
+    _PARAMS = ["param1"]
+
+    def _unnormalized_pdf(self, x):
+        """Documentation here."""
+        return [42.]
+
+
