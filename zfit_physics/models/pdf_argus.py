5cf8ac53fb020bf4d4aa08ff838f34cb0ecfcc02
Jonas Eschle <jonas.eschle@cern.ch>
2020-02-06 14:49:31 +0100

    Update pdf_argus.py
    Extracted pure function, zero elements outside

--- zfit_physics/models/pdf_argus.py
+++ zfit_physics/models/pdf_argus.py
@@ -1,12 +1,33 @@
 """ARGUS PDF (https://en.wikipedia.org/wiki/ARGUS_distribution)"""
+import tensorflow as tf
 import zfit
+from zfit import z
+
+
+@z.function_tf
+def argus_func(m, m0, c, p):
+    m_frac = m / m0
+    use_m_frac = tf.less(m_frac, z.constant(1.))
+
+    def argus_unsafe(m_frac):
+        m_factor = 1 - z.pow(m_frac, 2)
+        return m * z.pow(m_factor, p) * (z.exp(c * m_factor))
+
+    # if m > m0 (mass larger then the cutoff mass), this blows up. Set zero if m > m0
+    argus_filtered = z.safe_where(condition=use_m_frac,
+                                  func=argus_unsafe,
+                                  safe_func=lambda x: tf.zeros_like(x),
+                                  values=m_frac,
+                                  value_safer=lambda x: tf.ones_like(x) * 0.5,
+                                  )
+    return argus_filtered
 
 
 class Argus(zfit.pdf.ZPDF):
     """Implementation of the ARGUS PDF"""
 
     _N_OBS = 1
-    _PARAMS = "m0 c p".split()
+    _PARAMS = "m0", "c", "p"
 
     def _unnormalized_pdf(self, x):
         """
@@ -19,6 +40,4 @@ class Argus(zfit.pdf.ZPDF):
         c = self.params["c"]
         p = self.params["p"]
 
-        m_frac = m / m0
-        m_factor = 1 - zfit.ztf.pow(m_frac, 2)
-        return m * (m_factor ** p) * (zfit.ztf.exp(c * m_factor))
+        return argus_func(m, m0, c, p)
