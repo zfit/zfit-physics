d27e500fd0a7b4c07379e3bd25b228a97da3d87b
Jonas Eschle 'Mayou36 <mayou36@jonas.eschle.com>
2020-02-14 17:45:56 +0100

    Fix unittest, increase n points conv

--- tests/test_pdf_conv.py
+++ tests/test_pdf_conv.py
@@ -1,6 +1,7 @@
 """Example test for a pdf or function"""
 import pytest
 import zfit
+import tensorflow as tf
 # Important, do the imports below
 from zfit.core.testing import setup_function, teardown_function, tester
 
@@ -14,6 +15,7 @@ param2_true = 1.2
 
 def test_conv_simple():
     # test special properties  here
+    n_points = 2000
     obs = zfit.Space("obs1", limits=(-5, 5))
     param1 = zfit.Parameter('param1', -3)
     param2 = zfit.Parameter('param2', 0.3)
@@ -23,13 +25,13 @@ def test_conv_simple():
                                                    kernel=gauss1,
                                                    limits=obs, obs=obs)
 
-    x = np.linspace(-5, 5, 1000)
+    x = tf.linspace(-5., 5., n_points)
     probs = conv.pdf(x=x)
     integral = conv.integrate(limits=obs)
     probs_np = probs.numpy()
     assert pytest.approx(1, rel=1e-3) == integral.numpy()
-    assert len(probs_np) == 1000
+    assert len(probs_np) == n_points
     import matplotlib.pyplot as plt
-    plt.plot(x, probs_np)
-    plt.show()
+    # plt.plot(x, probs_np)
+    # plt.show()
     # assert len(conv.get_dependents(only_floating=False)) == 2  # TODO: activate again with fixed params
