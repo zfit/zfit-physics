0efd4cc0748ff1c04be713828c0305d95e2a6f7a
Simon Thor <simon11.thor@hotmail.se>
2021-08-30 21:37:17 +0200

    Added docstrings and JIT

--- zfit_physics/models/pdf_relbw.py
+++ zfit_physics/models/pdf_relbw.py
@@ -1,40 +1,53 @@
 import numpy as np
 import tensorflow as tf
 import zfit
+from zfit.util import ztyping
 from zfit import z
 from zfit.core.space import ANY_LOWER, ANY_UPPER, Space
 
 
+@z.function
 def arctan_complex(x):
-    """Function that evaluates arctan(x) using tensorflow but also supports complex numbers.
+    r"""Function that evaluates arctan(x) using tensorflow but also supports complex numbers.
+    It is defined as
+    .. math::
 
-    Args: x
+        \mathrm{arctan}(x) = \frac{i}{2} \left(\ln(1-ix) - \ln(1+ix)\right)
 
-    Returns: arctan(x)
+    Args:
+        x: tf.Tensor
 
-    Notes
-    -----
-    Formula used: https://www.wolframalpha.com/input/?i=arctan%28a%2Bb*i%29
+    Returns:
+        .. math:: \mathrm{arctan}(x)
+
+    Notes:
+        Formula is taken from https://www.wolframalpha.com/input/?i=arctan%28a%2Bb*i%29
+    TODO: move somewhere?
     """
-    return 1 / 2 * 1j * tf.math.log(1 - 1j * x) - 1 / 2 * 1j * tf.math.log(1 + 1j * x)
+    return 1 / 2 * 1j * (tf.math.log(1 - 1j * x) - tf.math.log(1 + 1j * x))
 
 
 class RelativisticBreitWigner(zfit.pdf.ZPDF):
     """Relativistic Breit-Wigner distribution.
 
     Formula for PDF and CDF are based on https://gist.github.com/andrewfowlie/cd0ed7e6c96f7c9e88f85eb3b9665b97
+
+    Args:
+        m: the average value
+        Gamma: the width of the distribution
     """
 
     _N_OBS = 1
     _PARAMS = ["m", "Gamma"]
 
-    def _unnormalized_pdf(self, x):
+    def _unnormalized_pdf(self, x: tf.Tensor) -> tf.Tensor:
         """Calculate the PDF at value(s) x.
 
-        Parameters
-        ----------
-        x : tf.Tensor
-            Energies. Either one value or an array
+        Args:
+            x : Either one value or an array
+
+        Returns:
+            `tf.Tensor`: The value(s) of the unnormalized PDF at x.
         """
         x = zfit.z.unstack_x(x)
         alpha = self.params["Gamma"] / self.params["m"]
@@ -50,29 +63,21 @@ class RelativisticBreitWigner(zfit.pdf.ZPDF):
         return k / ((x ** 2 - self.params["m"] ** 2) ** 2 + self.params["m"] ** 4 * alpha ** 2)
 
 
+@z.function
 def relbw_cdf_func(x, m, Gamma):
     """
-    Parameters
-    ----------
-    x
-    m : float
-        mass of resonance
-    Gamma : float
-        width of resonance
-    Returns
-    -------
-    cdf :  float
-        CDF of Breit-Wigner distribution
-    The CDf was found by Mathematica:
-    pdf = k/((m^2 - m^2)^2 + m^4*alpha^2)
-    cdf = Integrate[pdf, m]
-    >>> BW = breit_wigner(m=125., Gamma=0.05)
-    >>> BW.cdf(125.)
-    0.50052268648248666
-    >>> BW.cdf(1E10)
-    1.0000000000000004
-    >>> BW.cdf(0.)
-    0.0
+    Analytical function for the CDF of the relativistic Breit-Wigner distribution.
+
+    Args:
+         x: value(s) for which the CDF will be calculated.
+         m: Mean value
+         Gamma: width
+
+    Returns:
+        `tf.Tensor`: The calculated CDF values.
+
+    Notes:
+        Based on code from this [github gist](https://gist.github.com/andrewfowlie/cd0ed7e6c96f7c9e88f85eb3b9665b97#file-bw-py-L112-L154)
     """
     Gamma = z.to_complex(Gamma)
     m = z.to_complex(m)
@@ -93,13 +98,24 @@ def relbw_cdf_func(x, m, Gamma):
     return cdf_
 
 
-def relbw_integral(limits, params, model):
+def relbw_integral(limits: ztyping.SpaceType, params: dict, model) -> tf.Tensor:
+    """
+    Calculates the analytic integral of the relativistic Breit-Wigner PDF.
+
+    Args:
+        limits: An object with attribute rect_limits.
+        params: A hashmap from which the parameters that defines the PDF will be extracted.
+        model: Will be ignored.
+
+    Returns:
+        The calculated integral.
+    """
     lower, upper = limits.rect_limits
     lower_cdf = relbw_cdf_func(x=lower, m=params["m"], Gamma=params["Gamma"])
     upper_cdf = relbw_cdf_func(x=upper, m=params["m"], Gamma=params["Gamma"])
     return upper_cdf - lower_cdf
 
 
+# These lines of code adds the analytic integral function to RelativisticBreitWigner PDF.
 relbw_integral_limits = Space(axes=(0,), limits=(((ANY_LOWER,),), ((ANY_UPPER,),)))
-# TODO analytic integral does not work
 RelativisticBreitWigner.register_analytic_integral(func=relbw_integral, limits=relbw_integral_limits)
