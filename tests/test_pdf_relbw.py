24ea041d850e451cb8632b7e0ade9a89106b7004
Simon Thor <simon11.thor@hotmail.se>
2021-08-30 19:37:45 +0200

    Add analytic integral and corresponding tests

--- tests/test_pdf_relbw.py
+++ tests/test_pdf_relbw.py
@@ -1,5 +1,4 @@
 """Tests for relativistic Breit-Wigner PDF."""
-import numpy as np
 import pytest
 import tensorflow as tf
 import zfit
@@ -10,28 +9,42 @@ from zfit.core.testing import tester
 import zfit_physics as zphys
 
 # specify globals here. Do NOT add any TensorFlow but just pure python
-param1_true = 0.3
-param2_true = 1.2
+m_true = 125.
+Gamma_true = 0.05
 
 
-def test_pdf():
-    # Test PDF values here
-    obs = zfit.Space("obs1", (0, 200))
+def create_relbw(m, Gamma, limits):
+    obs = zfit.Space("obs1", limits)
+    relbw = zphys.pdf.RelativisticBreitWigner(m=m, Gamma=Gamma, obs=obs)
+    return relbw, obs
 
-    relbw = zphys.pdf.RelativisticBreitWigner(m=125.0, Gamma=0.05, obs=obs)
-    assert zfit.run(relbw.pdf(125.0)) == pytest.approx(12.7, rel=1e-3)
 
-    # analytic_integral = zfit.run(relbw.analytic_integrate(obs, norm_range=False))
-    # numeric_integral = zfit.run(relbw.numeric_integrate(obs, norm_range=False))
-    # assert pytest.approx(analytic_integral, 4e-2) == numeric_integral
+def test_relbw_pdf():
+    # Test PDF here
+    relbw, _ = create_relbw(m_true, Gamma_true, limits=(0, 200))
+    assert zfit.run(relbw.pdf(125.)) == pytest.approx(12.732396211295313, rel=1e-4)
+    assert relbw.pdf(tf.range(0., 200, 10_000)) <= relbw.pdf(125.)
+
+    sample = relbw.sample(1000)
+    tf.debugging.assert_all_finite(sample, 'Some samples from the relbw PDF are NaN or infinite')
+    assert sample.n_events == 1000
+    assert all(tf.logical_and(0 <= sample, sample <= 200))
+
+
+def test_relbw_integral():
+    # Test CDF and integral here
+    relbw, obs = create_relbw(m_true, Gamma_true, limits=(0, 200))
+
+    analytic_integral = zfit.run(relbw.analytic_integrate(obs, norm_range=False))[0]
+    numeric_integral = zfit.run(relbw.numeric_integrate(obs, norm_range=False))[0]
+    assert analytic_integral == pytest.approx(1., 1e-4)
+    assert numeric_integral == pytest.approx(1., 2e-3)
 
 
 # register the pdf here and provide sets of working parameter configurations
-
-
 def relbw_params_factory():
-    m = zfit.Parameter("m", 4.5)
-    Gamma = zfit.Parameter("Gamma", -2.3)
+    m = zfit.Parameter("m", m_true)
+    Gamma = zfit.Parameter("Gamma", Gamma_true)
     return {"m": m, "Gamma": Gamma}
 
 
--- zfit_physics/models/pdf_relbw.py
+++ zfit_physics/models/pdf_relbw.py
@@ -1,16 +1,36 @@
-import numpy as np
-import tensorflow as tf
 import zfit
-from zfit import z
 from zfit.core.space import ANY_LOWER, ANY_UPPER, Space
+from zfit import z
+import tensorflow as tf
+import numpy as np
+
+
+def arctan_complex(x):
+    """
+    Function that evaluates arctan(x) using tensorflow but also supports complex numbers.
+
+    Args: x
+
+    Returns: arctan(x)
+
+    Notes
+    -----
+    Formula used: https://www.wolframalpha.com/input/?i=arctan%28a%2Bb*i%29
+    """
+    return 1/2 * 1j * tf.math.log(1 - 1j*x) - 1/2 * 1j * tf.math.log(1 + 1j*x)
 
 
 class RelativisticBreitWigner(zfit.pdf.ZPDF):
-    _N_OBS = 1  # dimension, can be omitted
-    _PARAMS = ["m", "Gamma"]  # the name of the parameters
+    """
+    Relativistic Breit-Wigner distribution.
+    Formula for PDF and CDF are based on https://gist.github.com/andrewfowlie/cd0ed7e6c96f7c9e88f85eb3b9665b97
+    """
+    _N_OBS = 1
+    _PARAMS = ['m', 'Gamma']
 
     def _unnormalized_pdf(self, x):
-        """Calculate the PDF at value(s) x.
+        """
+        Calculate the PDF at value(s) x.
 
         Parameters
         ----------
@@ -18,17 +38,11 @@ class RelativisticBreitWigner(zfit.pdf.ZPDF):
             Energies. Either one value or an array
         """
         x = zfit.z.unstack_x(x)
-        alpha = self.params["Gamma"] / self.params["m"]
-        gamma = self.params["m"] ** 2 * (1.0 + alpha ** 2) ** 0.5
-        k = (
-            2.0 ** (3.0 / 2.0)
-            * self.params["m"] ** 2
-            * alpha
-            * gamma
-            / (np.pi * (self.params["m"] ** 2 + gamma) ** 0.5)
-        )
+        alpha = self.params['Gamma'] / self.params['m']
+        gamma = self.params['m']**2 * (1. + alpha**2)**0.5
+        k = 2.**(3. / 2.) * self.params['m']**2 * alpha * gamma / (np.pi * (self.params['m']**2 + gamma)**0.5)
 
-        return k / ((x ** 2 - self.params["m"] ** 2) ** 2 + self.params["m"] ** 4 * alpha ** 2)
+        return k / ((x**2 - self.params['m']**2)**2 + self.params['m']**4 * alpha**2)
 
 
 def relbw_cdf_func(x, m, Gamma):
@@ -55,15 +69,19 @@ def relbw_cdf_func(x, m, Gamma):
     >>> BW.cdf(0.)
     0.0
     """
+    Gamma = z.to_complex(Gamma)
+    m = z.to_complex(m)
+    x = z.to_complex(x)
+
     alpha = Gamma / m
-    gamma = m ** 2 * (1.0 + alpha ** 2) ** 0.5
-    k = 2.0 ** (3.0 / 2.0) * m ** 2 * alpha * gamma / (np.pi * (m ** 2 + gamma) ** 0.5)
+    gamma = m ** 2 * (1. + alpha ** 2) ** 0.5
+    k = 2. ** (3. / 2.) * m ** 2 * alpha * gamma / (np.pi * (m ** 2 + gamma) ** 0.5)
 
-    arg_1 = z.to_complex(-1) ** (1.0 / 4.0) / (-1j + alpha) ** 0.5 * x / m
-    arg_2 = z.to_complex(-1) ** (3.0 / 4.0) / (1j + alpha) ** 0.5 * x / m
+    arg_1 = z.to_complex(-1) ** (1. / 4.) / (-1j + alpha) ** 0.5 * x / m
+    arg_2 = z.to_complex(-1) ** (3. / 4.) / (1j + alpha) ** 0.5 * x / m
 
-    shape = -1j * tf.math.atan(arg_1) / (-1j + alpha) ** 0.5 - tf.math.atan(arg_2) / (1j + alpha) ** 0.5
-    norm = z.to_complex(-1) ** (1.0 / 4.0) * k / (2.0 * alpha * m ** 3)
+    shape = -1j * arctan_complex(arg_1) / (-1j + alpha) ** 0.5 - arctan_complex(arg_2) / (1j + alpha) ** 0.5
+    norm = z.to_complex(-1) ** (1. / 4.) * k / (2. * alpha * m ** 3)
 
     cdf_ = shape * norm
     cdf_ = z.to_real(cdf_)
@@ -72,11 +90,11 @@ def relbw_cdf_func(x, m, Gamma):
 
 def relbw_integral(limits, params, model):
     lower, upper = limits.rect_limits
-    lower_cdf = relbw_cdf_func(x=limits[0], m=params["m"], Gamma=params["Gamma"])
-    upper_cdf = relbw_cdf_func(x=limits[1], m=params["m"], Gamma=params["Gamma"])
-    return upper - lower
+    lower_cdf = relbw_cdf_func(x=lower, m=params['m'], Gamma=params['Gamma'])
+    upper_cdf = relbw_cdf_func(x=upper, m=params['m'], Gamma=params['Gamma'])
+    return upper_cdf - lower_cdf
 
 
 relbw_integral_limits = Space(axes=(0,), limits=(((ANY_LOWER,),), ((ANY_UPPER,),)))
 # TODO analytic integral does not work
-# RelativisticBreitWigner.register_analytic_integral(func=relbw_integral, limits=relbw_integral_limits)
+RelativisticBreitWigner.register_analytic_integral(func=relbw_integral, limits=relbw_integral_limits)
