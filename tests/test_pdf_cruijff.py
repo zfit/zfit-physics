56bf1057d67aafa9dc7364faaa1301a114f81aab
iasonkrom <iason.krom@gmail.com>
2024-03-19 00:39:24 +0100

    add cruijff pdf

---
+++ tests/test_pdf_cruijff.py
@@ -0,0 +1,91 @@
+"""Tests for Cruijff PDF."""
+
+import numpy as np
+import pytest
+import tensorflow as tf
+import zfit
+from numba_stats import cruijff as cruijff_numba
+from scipy import integrate
+
+# Important, do the imports below
+from zfit.core.testing import tester
+
+import zfit_physics as zphys
+
+# specify globals here. Do NOT add any TensorFlow but just pure python
+mu_true = 90.0
+sigmal_true = 5.0
+alphal_true = 3.0
+sigmar_true = 10.0
+alphar_true = 2.0
+
+
+def create_cruijff(mu, sigmal, alphal, sigmar, alphar, limits):
+    obs = zfit.Space("obs1", limits=limits)
+    cruijff = zphys.pdf.Cruijff(mu=mu, sigmal=sigmal, alphal=alphal, sigmar=sigmar, alphar=alphar, obs=obs, norm=False)
+    return cruijff, obs
+
+
+def test_cruijff_pdf():
+    # Test PDF hereA
+    cruijff, _ = create_cruijff(
+        mu=mu_true, sigmal=sigmal_true, alphal=alphal_true, sigmar=sigmar_true, alphar=alphar_true, limits=(50, 130)
+    )
+    assert zfit.run(cruijff.pdf(90.0)) == pytest.approx(
+        cruijff_numba.density(
+            90.0,
+            beta_left=alphal_true,
+            beta_right=alphar_true,
+            loc=mu_true,
+            scale_left=sigmal_true,
+            scale_right=sigmar_true,
+        ).item(),
+        rel=1e-8,
+    )
+    np.testing.assert_allclose(
+        cruijff.pdf(tf.range(50.0, 130, 10_000)),
+        cruijff_numba.density(
+            tf.range(50.0, 130, 10_000).numpy(),
+            beta_left=alphal_true,
+            beta_right=alphar_true,
+            loc=mu_true,
+            scale_left=sigmal_true,
+            scale_right=sigmar_true,
+        ),
+        rtol=1e-8,
+    )
+    assert cruijff.pdf(tf.range(50.0, 130, 10_000)) <= cruijff.pdf(90.0)
+
+
+def test_cruihff_integral():
+    # Test CDF and integral here
+    cruijff, obs = create_cruijff(
+        mu=mu_true, sigmal=sigmal_true, alphal=alphal_true, sigmar=sigmar_true, alphar=alphar_true, limits=(50, 130)
+    )
+    full_interval_numeric = zfit.run(cruijff.numeric_integrate(obs, norm_range=False))
+    true_integral = 67.71494
+    numba_stats_full_integral = integrate.quad(
+        cruijff_numba.density, 50, 130, args=(alphal_true, alphar_true, mu_true, sigmal_true, sigmar_true)
+    )[0]
+    assert full_interval_numeric == pytest.approx(true_integral, 1e-7)
+    assert full_interval_numeric == pytest.approx(numba_stats_full_integral, 1e-7)
+
+    numeric_integral = zfit.run(cruijff.numeric_integrate(limits=(80, 100), norm_range=False))
+    numba_stats_integral = integrate.quad(
+        cruijff_numba.density, 80, 100, args=(alphal_true, alphar_true, mu_true, sigmal_true, sigmar_true)
+    )[0]
+    assert numeric_integral == pytest.approx(numba_stats_integral, 1e-7)
+
+
+# register the pdf here and provide sets of working parameter configurations
+def cruijff_params_factory():
+    mu = zfit.Parameter("mu", mu_true)
+    sigmal = zfit.Parameter("sigmal", sigmal_true)
+    alphal = zfit.Parameter("alphal", alphal_true)
+    sigmar = zfit.Parameter("sigmar", sigmar_true)
+    alphar = zfit.Parameter("alphar", alphar_true)
+
+    return {"mu": mu, "sigmal": sigmal, "alphal": alphal, "sigmar": sigmar, "alphar": alphar}
+
+
+tester.register_pdf(pdf_class=zphys.pdf.Cruijff, params_factories=cruijff_params_factory)
--- zfit_physics/pdf.py
+++ zfit_physics/pdf.py
@@ -1,5 +1,6 @@
 from .models.pdf_argus import Argus
 from .models.pdf_cmsshape import CMSShape
+from .models.pdf_cruijff import Cruijff
 from .models.pdf_relbw import RelativisticBreitWigner
 
-__all__ = ["Argus", "RelativisticBreitWigner", "CMSShape"]
+__all__ = ["Argus", "RelativisticBreitWigner", "CMSShape", "Cruijff"]
