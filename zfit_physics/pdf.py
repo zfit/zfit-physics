633939c06f43363b71a9ee624bb04f7955ae4fe8
iasonkrom <iason.krom@gmail.com>
2024-03-28 11:30:55 -0500

    ok I lied, this is final :

--- zfit_physics/pdf.py
+++ zfit_physics/pdf.py
@@ -5,11 +5,4 @@ from .models.pdf_erfexp import ErfExp
 from .models.pdf_novosibirsk import Novosibirsk
 from .models.pdf_relbw import RelativisticBreitWigner
 
-__all__ = [
-    "Argus",
-    "RelativisticBreitWigner",
-    "CMSShape",
-    "Cruijff",
-    "ErfExp",
-    "Novosibirsk",
-]
+__all__ = ["Argus", "RelativisticBreitWigner", "CMSShape", "Cruijff", "ErfExp", "Novosibirsk"]
