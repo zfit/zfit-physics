b260af3f12f98fe4215f41aed849535ead6cafbb
Iason Krommydas <iason.krom@gmail.com>
2024-04-01 15:44:20 -0500

    feat: add `Tsallis` PDF (#70)
    * first attempt
    
    * update changelog
    
    * forgot to add the file
    
    * fixes
    
    * add testing
    
    * typo
    
    * add parameter check at class instantiation
    
    * Update zfit_physics/models/pdf_tsallis.py
    
    Co-authored-by: Jonas Eschle <jonas.eschle@cern.ch>
    
    * Update zfit_physics/models/pdf_tsallis.py
    
    Co-authored-by: Jonas Eschle <jonas.eschle@cern.ch>
    
    * add formula ref
    
    * fix
    
    ---------
    
    Co-authored-by: Jonas Eschle <jonas.eschle@cern.ch>

--- zfit_physics/pdf.py
+++ zfit_physics/pdf.py
@@ -3,5 +3,6 @@ from .models.pdf_cmsshape import CMSShape
 from .models.pdf_cruijff import Cruijff
 from .models.pdf_erfexp import ErfExp
 from .models.pdf_relbw import RelativisticBreitWigner
+from .models.pdf_tsallis import Tsallis
 
-__all__ = ["Argus", "RelativisticBreitWigner", "CMSShape", "Cruijff", "ErfExp"]
+__all__ = ["Argus", "RelativisticBreitWigner", "CMSShape", "Cruijff", "ErfExp", "Tsallis"]
